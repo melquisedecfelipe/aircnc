d6f8e5cb7a587b38eeb92c3f1e95290afa10208a
Melquisedec Felipe <melquisedeccoutinho@finchsolucoes.com.br>
2019-10-03 17:30:54 -0300

    finsh backend

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0-nejwy.mongodb.net/admin?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edicao)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
